946aeb2397584d587f56e61148b03ed2456e2b69
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-07-27 13:25:15 +0300

    Update treble_arm64_bN_slim.mk

--- treble_arm64_bN_slim.mk
+++ treble_arm64_bN_slim.mk
@@ -1,11 +1,10 @@
-TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
 
-$(call inherit-product, device/phh/treble/pixel.mk)
+$(call inherit-product, device/phh/treble/everest.mk)
 
-PRODUCT_NAME := treble_arm64_bN_slim
+PRODUCT_NAME := treble_arm64_bvN
 PRODUCT_DEVICE := tdgsi_arm64_ab
 PRODUCT_BRAND := google
 PRODUCT_SYSTEM_BRAND := google
